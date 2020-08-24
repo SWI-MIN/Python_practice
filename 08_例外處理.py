589060522adbb0611de8baccc25202d5f38d7965
swimming <swimming881127@gmail.com>
2020-08-24 22:09:41 +0800

    upload

--- 08_例外處理.py
+++ 08_例外處理.py
@@ -1,8 +1,9 @@
-try:
-    a = int(input('pls intput a number : '))
-    b = int(input('pls intput a number : '))
-    r = a % b
-except (ValueError,ZeroDivisionError) as err:
-    print('find {} 0 error' .format(err))
-else:
-    print('r = ',r)
+while(True):
+    try:
+        a = int(input('pls intput a number : '))
+        b = int(input('pls intput a number : '))
+        r = a % b
+    except (ValueError,ZeroDivisionError) as err:
+        print('find {} error' .format(err))
+    else:
+        print('r = ',r)
---
+++ 09_使用者介面.py
@@ -0,0 +1,7 @@
+import tkinter as tk
+
+swim = tk.Tk()
+
+swim.geometry('720*240')
+swim.title('TITLE')
+swim.mainloop()
